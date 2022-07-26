e7a8fce0947f11567b5d627c750c913df46c6350
Diener Dornelas <diener.ld@outlook.com>
2022-07-25 22:22:55 -0300

    add fetch pokemon

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,8 +1,33 @@
+import { Typography } from "@mui/material";
 import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
+import { getPokemon } from "../../services/api";
+
+type TPokemon = {
+  name: string
+}
 
 function Home() {
+  const [pokemon, setPokemon] = useState<TPokemon>({} as TPokemon);
+
+  useEffect(()=>{
+    const get = async()=>{
+      const response = await getPokemon(11);
+      setPokemon(response);
+    }
+    get();
+  },[])
+
+  useEffect(()=>{
+    console.log(pokemon);
+  },[pokemon])
+
+
   return (
     <>
+    <Typography variant="body2">
+      {pokemon?.name}
+    </Typography>
       <Button variant="contained">Pesquisar</Button>
     </>
   );
