a2e1a67b0368d3461d0a1f899c030d4540d39c38
Carina Abreu <carina.vas.abreu@gmail.com>
2022-07-25 21:39:23 -0300

    inserção página home

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Roteador from "./Routes";
+
+function App() {
+  return (
+    <>
+      <Roteador />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import Button from "@mui/material/Button";
+
+function Home() {
+  return (
+    <>
+      <Button variant="contained">Pesquisar</Button>
+    </>
+  );
+}
+
+export default Home;
