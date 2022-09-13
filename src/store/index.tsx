35b2c20265572dede23d3a837c7e6cca1b1b8af8
kelly-guedes <guedes.kelly@icloud.com>
2022-09-13 19:33:43 -0300

    inicializado atv

--- src/store/index.tsx
+++
@@ -1,5 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import combineReducers from './modules/rootReducer'
-
-export const store = configureStore({reducer: combineReducers});
-export type AppDispatch = typeof store.dispatch;
--- src/store/modules/rootReducer.tsx
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import pokemon from './pokemon/pokemonSlice';
-
-const reducers = combineReducers({
-    pokemon
-});
-
-export default reducers;
-export type State = ReturnType<typeof reducers>;
